cc725eaacd1976aea21c5bbd90d2259a4bdfc857
mellewessels <113339030+mellewessels@users.noreply.github.com>
2023-01-13 12:42:35 +0100

    aahahaahahhahhhhaaaæ

--- Game.py
+++ Game.py
@@ -1,11 +1,16 @@
 import sys, pygame
 pygame.init
+white = (255,255,255)
 Screen = pygame.display.set_mode((800, 600))
 
+bg_img = pygame.image.load('images/bg.png')
+bg_img = pygame.transform.scale(bg_img,(800,600))
+Screen.blit(bg_img,(0, 0))
 while True:
-
+    Screen.fill(white)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
          pygame.quit()
    
-    pygame.display.flip
+    pygame.display.update
+
