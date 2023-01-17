de3987b18b584c67b4b6172d5b049af19ca8bc4e
BruceVista <513077@vistacollege.nl>
2023-01-17 20:08:45 +0100

    qdas
    adsasd

--- test.py
+++ test.py
@@ -1,88 +1,103 @@
 import sys, pygame
-pygame.init
+from pygame.locals import *
+
+pygame.init()
+
+screen_width = 1000
+screen_height = 1000
 
 
 
-#Screen
-red = (255,0 ,0)
-white = (255, 255, 255)
-Screen = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Platformer')
+
+tile_size = 50
+def draw_grid():
+	for line in range(0, 20):
+		pygame.draw.line(screen, (255, 255, 255), (0, line * tile_size), (screen_width, line * tile_size))
+		pygame.draw.line(screen, (255, 255, 255), (line * tile_size, 0), (line * tile_size, screen_height))
+
+
+
+class World():
+    def __init__(self, data):
+
+        self.tile_list = []
+        dirt_img = pygame.image.load('images/platform.png')
+        grass_img = pygame.image.load('images/grass.png')
+        row_count = 0
+        for row in data: 
+            col_count = 0
+            for tile in row:
+                if tile == 1: 
+                    img = pygame.transform.scale(dirt_img, (tile_size, tile_size))
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
+                if tile == 2: 
+                    img = pygame.transform.scale(grass_img, (tile_size, tile_size))
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
+                col_count += 1
+            row_count += 1
+
+
+    def draw(self):
+        for tile in self.tile_list:
+         screen.blit(tile[0], tile[1])    
+
+
+world_data = [
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+] 
+
+
+world = World(world_data)
+
+
+
+#load images
 bg = pygame.image.load('images/bg.png')
-bg = pygame.transform.scale(bg, (800, 600))
 
-#Platform and floor
-Floor = pygame.image.load('images/platform.png')
-Floor = pygame.transform.scale(Floor,(1000, 200))
-P1X = 300
-P1Y = 350
-Platform1 = pygame.image.load('images/platform.png')
-Platform1 = pygame.transform.scale(Platform1,(100, 20))
-collision = False
-#character
-Jumping = False
+run = True
+while run:
 
-Move_Speed = 10
-Y_Gravity = 1
-Jump_Height = 20
-Y_Velocity = Jump_Height
+    screen.blit(bg, (0, 0))
 
-PlatformCollision = P1Y - Jump_Height
+    world.draw()
 
-character = pygame.image.load('images/character.png')
-character = pygame.transform.scale(character, (100, 100))
-character_jumping = pygame.image.load('images/Character.png')
-character_jumping = pygame.transform.scale(character_jumping, (100, 100))
-CharX = 350
-CharY = 450
-character_rect = character.get_rect(center=(CharX, CharY))
+    draw_grid()
 
-running = True
-while running:
-    
-    Screen.fill(white)
-    Screen.blit(bg ,(0, 0))
-    Screen.blit(Floor,(-100, 500))
-    Screen.blit(Platform1,(P1X, P1Y))
-    for event in pygame.event.get():
+    for event in pygame.event.get ():
         if event.type == pygame.QUIT:
-         running = False
+            run = False
 
-    #jump
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        Jumping = True
-    
-    if Jumping == True:
-        CharY -= Y_Velocity
-        Y_Velocity -= Y_Gravity
-        character_rect = character.get_rect(center=(CharX, CharY))
-        Screen.blit(character, character_rect)   
-        if Y_Velocity < -Jump_Height:
-             Jumping = False
-             Y_Velocity = Jump_Height
-             character_rect = character.get_rect(center=(CharX, CharY))
-             Screen.blit(character_jumping, character_rect)
-    else: 
-        character_rect = character.get_rect(center=(CharX, CharY))
-        Screen.blit(character, character_rect)    
-    
-    P1Col = pygame.draw.rect(Screen, red, pygame.Rect(P1X, P1Y+5, 100, 15),2) 
-    CharCol = pygame.draw.rect(Screen, red, pygame.Rect(CharX-30, CharY+50, 60, 1),2)
-    collide = pygame.Rect.colliderect(P1Col, CharCol)
-    if CharX >= P1X and CharX <= P1X+10:
-        CharY = P1Y-50
-    if not CharX >= P1X and CharX <= P1X+100:
-        CharY = 450
-
-
-    #Movement
-    
-    if keys[pygame.K_a]:
-        P1X += Move_Speed
-    if keys[pygame.K_d]:
-        P1X -= Move_Speed
-
-
+  
+  
     pygame.display.update()
-    pygame.time.Clock().tick(60)
+
 pygame.quit
